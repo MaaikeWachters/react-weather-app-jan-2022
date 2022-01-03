4f04bb0052b004023cd0f4ac19d846f20b28d248
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-01-03 09:27:15 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded with 💖 &nbsp;by{" "}
-        <a
-          href="http://www.maaikewachters.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Maaike Wachters
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/MaaikeWachters/react-weather-app-jan-2022"
-          target="_blank"
-          rel="noreferrer"
-        >
-          
-        GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This app was coded with 💖 &nbsp;by{" "}
+          <a
+            href="http://www.maaikewachters.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Maaike Wachters
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/MaaikeWachters/react-weather-app-jan-2022"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
