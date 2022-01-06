c410c5c5094cf33d4212d09dc877e651c5710d91
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-01-06 15:58:51 +0100

    Fixed some layout issues

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="rebeccapurple"
-      size={48}
+      size={64}
       animate={true}
     />
   );
